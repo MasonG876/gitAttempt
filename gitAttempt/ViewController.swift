d1e59ec6b4d9ecbaf9905154fdc772b14eb12b27
Mason Gardner <mgardner2022@STEAM-7.local>
2019-04-03 11:32:58 -0700

    second edit

--- gitAttempt/ViewController.swift
+++ gitAttempt/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var aerwghio: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
